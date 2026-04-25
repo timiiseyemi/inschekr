b06885fe9b9a4459c3e1597e1a35e8be942a7184
timmy quan <timmyquan123@aol.com>
2026-04-25 07:47:41 -0700

    New Commit

--- src/app/_imgtest/page.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-
-export default function ImgTest() {
-  return (
-    <div style={{ padding: 40, fontSize: 20 }}>
-      <h1>_imgtest is working 🎉</h1>
-    </div>
-  );
-}
--- src/app/test/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function TestPage() {
-  return (
-    <div style={{ padding: 40, fontSize: 24 }}>
-      TEST ROUTE WORKING ✔✔✔
-    </div>
-  );
-}
